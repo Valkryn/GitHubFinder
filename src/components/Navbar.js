748c7469feea8ea22f2d2dfdf099f7c0c13b4e54
Valkryn <calvinraphaelg@gmail.com>
2020-12-07 12:05:13 -0500

    nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+class Navbar extends React.Component{
+
+  render(){
+    return(
+      <div className= "navbar bg-primary">
+        <h1>
+            <i class="fab fa-github"></i>
+          Navbar
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
